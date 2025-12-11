82828b8fd48dbb44828eac237a80827a0efb3b61
IamMe <152034114+IAMME543@users.noreply.github.com>
2025-12-11 16:15:39 +1030

    Fix syntax error in image-masonry.js

--- image-masonry.js
+++ image-masonry.js
@@ -10,7 +10,7 @@
 
   
 
-  columncount.forEach(col = () -> {
+  columncount.forEach(col = () => {
     column = document.createElement('div');
     column.classList.add('column');
     columns.push(column);
